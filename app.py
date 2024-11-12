937c418ceea76a3fb9028cf9f6136686c9a15748
WidheliaPramesthy <widheliapramesthy@gmail.com>
2024-11-12 15:53:06 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+# --- PAGE SETUP ---
+home_page = st.Page(
+    page="views/home.py",
+    title="Home",
+    icon=":material/home:",
+    default=True,
+)
+chatbot_page = st.Page(
+    page="views/chatbot.py",
+    title="Chatbot Assistant",
+    icon=":material/smart_toy:"
+)
+dashboard_page = st.Page(
+    page="views/dashboard.py",
+    title="Dashboard Monitoring",
+    icon=":material/team_dashboard:"
+)
+
+informasi_page = st.Page(
+    page="views/informasi.py",
+    title="Infromasi",
+    icon=":material/info:"
+)
+
+# --- NAVIGATION SETUP ---
+pg = st.navigation(pages=[home_page, informasi_page, chatbot_page, dashboard_page], expanded=False)
+
+# --- RUN NAVIGATION ---
+pg.run()
